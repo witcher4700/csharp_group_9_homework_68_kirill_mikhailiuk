ef0078b54c83d2b5f7253de34323724d3c154118
Kirill Mikhailiuk <witcher470@gmail.com>
2021-10-20 06:46:57 +0600

    Добавление образования, опыта, резюме и вакансии

--- HeadHunter/Controllers/ResumeController.cs
+++ HeadHunter/Controllers/ResumeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HeadHunter.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,75 @@ namespace HeadHunter.Controllers
 {
     public class ResumeController : Controller
     {
+        private HeadHunterContext _context;
+        public ResumeController(HeadHunterContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Add(Resume resume)
+        {
+            if (resume != null)
+            {
+                var userName = User.Identity.Name;
+                resume.UserId = _context.Users.FirstOrDefault(u => u.UserName == userName).Id;
+                resume.RefreshDate = DateTime.Now;
+                resume.Status = Status.InStock;
+                _context.Resumes.Add(resume);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult AddEducation(int resumeId)
+        {
+            var education = new Education()
+            {
+                ResumeId = resumeId
+            };
+            return View(education);
+        }
+
+        [HttpPost]
+
+        public IActionResult AddEducation(Education education)
+        {
+            if (education != null)
+            {
+                _context.Educations.Add(education);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult AddExpirience(int resumeId)
+        {
+            var education = new Education()
+            {
+                ResumeId = resumeId
+            };
+            return View(education);
+        }
+
+        [HttpPost]
+
+        public IActionResult AddExpirience(Expirience expirience)
+        {
+            if (expirience != null)
+            {
+                _context.Expiriences.Add(expirience);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- HeadHunter/Controllers/VacancyController.cs
+++ HeadHunter/Controllers/VacancyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HeadHunter.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,35 @@ namespace HeadHunter.Controllers
 {
     public class VacancyController : Controller
     {
+        private HeadHunterContext _context;
+        public VacancyController(HeadHunterContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Add(Vacancy vacancy)
+        {
+            if (vacancy != null)
+            {
+                var userName = User.Identity.Name;
+                vacancy.UserId = _context.Users.FirstOrDefault(u => u.UserName == userName).Id;
+                vacancy.RefreshDate = DateTime.Now;
+                vacancy.Status = Status.InStock;
+                _context.Vacancies.Add(vacancy);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
